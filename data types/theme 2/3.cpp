313a9a4ee176319ef6bb69d43380e10b31777bc9
Skapar <115580193+Skapar@users.noreply.github.com>
2022-11-01 22:25:17 +0400

    Add files via upload

---
+++ data types/theme 2/3.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    char c;
+    cin >> c;
+    if(c>='a' && c<='z'){
+        c-=32;
+    }
+    else if(c>='A' && c<'Z'){
+        c+=32;
+    }
+    cout << c;
+	
+
+}
