cad362bc4e2295bd44ed0a6f9694d3db12ad377b
Skapar <115580193+Skapar@users.noreply.github.com>
2022-11-01 22:32:45 +0400

    Add files via upload

---
+++ data types/theme 2/4.cpp
@@ -0,0 +1,16 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main() {
+    string c,b;
+    cin >> c >> b;
+
+    if(c==b){
+        cout << "yes";
+    }
+    else{
+        cout << "no";
+    }
+	
+
+}
